a53615ff69d652f79861afd1f518559a81dda723
Anton Uteev <uteev@bk.ru>
2017-06-14 19:18:58 +0300

    first commit

---
+++ HW1/src/main/java/uteevbkru/Homework1.java
@@ -0,0 +1,12 @@
+package uteevbkru;
+
+/**
+ * Created by anton on 22.05.17.
+ */
+public class Homework1 {
+
+    public static void main(String []args) {
+        System.out.println("This first homework for OTUS!");
+    }
+
+}
