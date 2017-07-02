e2607d2f97419a9cb46dc0cab6d10123f465f367
Anton Uteev <uteev@bk.ru>
2017-07-02 08:28:11 +0300

    add

--- HW4_/src/main/java/uteevbkru/MapLeaker.java
+++ HW4_/src/main/java/uteevbkru/MapLeaker.java
@@ -17,31 +17,22 @@ import javax.management.openmbean.CompositeData;
  */
 public class MapLeaker {
 
-
     public ExecutorService exec = Executors.newFixedThreadPool(5);
     public Map<Task, TaskStatus> taskStatus
             = Collections.synchronizedMap(new HashMap<Task, TaskStatus>());
-    private Random random = new Random();
-    private static final Logger log = Logger.getLogger(MapLeaker.class);
 
+    private static final Logger log = Logger.getLogger(MapLeaker.class);
     private enum TaskStatus { NOT_STARTED, STARTED, FINISHED };
 
     private class Task implements Runnable {
-        private int[] numbers = new int[random.nextInt(200)];
-
         public void run() {
-            //System.out.println("B"+ " ");
-
-            int[] temp = new int[random.nextInt(10000)];
             taskStatus.put(this, TaskStatus.STARTED);
             doSomeWork();
             taskStatus.put(this, TaskStatus.FINISHED);
         }
-
     }
     public Task newTask() {
         Task t = new Task();
-        //System.out.print("A" + " ");
         taskStatus.put(t, TaskStatus.NOT_STARTED);
         exec.execute(t);
         return t;
